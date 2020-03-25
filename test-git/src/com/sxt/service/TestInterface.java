10555f45673d0a25f93c42e2d2c3e12280c32a03
linyuanjun <2108861492@qq.com>
2020-03-25 15:34:54 +0800

    commit test

--- test-git/src/com/sxt/service/TestInterface.java
+++ test-git/src/com/sxt/service/TestInterface.java
@@ -23,7 +23,19 @@ public interface TestInterface {
 	 * 程序员使用。
 	 * 新建分支或切换分支：
 	 * 右键项目-team-switch to->New Branch或者切换到其他分支
-	 * 合并分支：
+	 * 合并分支：merge
+	 * 删除分支：先删除本地分支：advanced ->delete branch
+	 * 再删除远程分支：要从Remote->push...里面添加删除的选项才可以
 	 * 
+	 * 冲突解决：synchronsize workspace
+	 * 拉取内容：拉取远程工程 右键远程工程import project
+	 * 删除代码：在本地删除代码，然后提交到远程同步
+	 * 忽略文件：如果本地有若干文件或目录不想提交到远程，可以使用忽略的方式实现
+	 * .gitignore文件中定义的内容，都是忽略的内容，不上传到远程仓库的内容
+	 * 目录-/目录名称/
+	 * 文件-/文件名称...，/目录名称/文件名称
+	 * /代表工程的根目录
+	 * 修改.gitignore文件后，要将修改后的文件添加到索引：team->add to index
+	 * 先添加了再提交commit配置保证忽略信息生效，然后再上传的时候才会生效
 	 */
 }
